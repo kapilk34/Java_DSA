d0bbdd4dfafa3aef6e65b58eb601e8f3c303d930
kapilk34 <kapilkumargupta1511@gmail.com>
2026-01-20 19:33:56 +0530

    new questions in array

---
+++ Arrays/Reverse.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class Reverse {
+    public static void Reversing(int number[]){
+        int first = 0;
+        int last = number.length-1;
+
+        while (first < last){
+            //swaping the numbers using the temp variable
+            // int temp = number[last];
+            // number[last] = number[first];
+            // number[first] = temp;
+
+
+            //swaping the numbers using the temp variable
+            number[first] = number[first] + number[last];
+            number[last]  = number[first] - number[last];
+            number[first] = number[first] - number[last];
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2,1,8,4,9,3};
+        Reversing(number);
+
+        for(int i = 0; i < number.length; i++){
+            System.out.print(number[i]+ " ");
+        }
+    }
+}
