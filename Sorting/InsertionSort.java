04d71d9a094146274243384c0e4a9048567230aa
kapilk34 <kapilkumargupta1511@gmail.com>
2026-02-09 00:13:54 +0530

    sorting techiques

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = {2,5,3,7,1,4};
+        int n = nums.length;
+        for(int i = 0; i <= n-1; i++){
+            for(int j = i; j >= 1; j--){
+                if(nums[j] < nums[j-1]){
+                    //swapping the elements of nums[j] with nums[j-1]
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
+                else break;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {1,4,5,2,7,-3,9,6,8};
+        int n = nums.length;
+        for(int i = 0; i <= n-1; i++){
+            int min = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for(int j = i; j < n; j++){
+                if(nums[j] < min){
+                    min = nums[j];
+                    minIndex = j;
+                }
+            }
+            //swap nums[i] with minIndex
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        //printing the sorted array
+        for(int i = 0; i < n; i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
+
+
+/* 1. Slection sort is unstable sort
+   2. It can not be optimised
+   3. Number of swaps are less
+   4. n-1 swaps are fixed
+*/
