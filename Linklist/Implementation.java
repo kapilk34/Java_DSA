6a56040e5b87972b7805009f30c6ba8ef3fa1c62
kapilk34 <kapilkumargupta1511@gmail.com>
2026-03-23 10:52:37 +0530

    LinkedList practice Questions

--- Linklist/Implementation.java
+++ Linklist/Implementation.java
@@ -22,6 +22,14 @@ class Linkedlist{ //user define data structure
         return -1;
     }
 
+    int get(int index){
+        Node temp = head;
+        for(int i = 1; i < index; i++){
+            temp = temp.next;
+        }
+        return temp.val; 
+    }
+
     void addAtTail(int val){
         Node temp = new Node(val);
         if(tail == null){
@@ -65,6 +73,43 @@ class Linkedlist{ //user define data structure
         if(head == null) tail = null;
         size--;
     }
+
+    void insertAtIndex(int val, int index){
+        if(index < 0 || index > size){
+            System.out.println("Invalid index");
+            return;
+        }
+        if(index == 0) addAtHead(val);
+        else if( index == size) addAtTail(val);
+        else{
+            Node temp = head;
+            for(int i = 1; i < index-1; i++){
+                temp = temp.next;
+            }
+            Node newNode = new Node(val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+    }
+
+    void deleteAtIndex(int index){
+        if(index < 0 || index >= size){
+            System.out.println("Invalid index");
+            return;
+        }
+        if(index == 0){
+            deleteAtHead();
+            return;
+        }
+        Node temp = head;
+        for(int i = 1; i < index-1; i++){
+            temp = temp.next;
+        } 
+        temp.next = temp.next.next; // deleted the connection from the index node
+        if(index == size-1) tail = temp; // to delete the tail node
+        size--;
+    }
 }
 
 public class Implementation {
@@ -74,11 +119,19 @@ public class Implementation {
         list.addAtTail(20); 
         list.addAtTail(30); 
         list.addAtTail(40); 
+        list.addAtTail(50); 
+        list.addAtTail(80); 
+        list.addAtTail(90); 
         list.displayList();
         list.addAtHead(60);
         list.displayList();
         list.deleteAtHead();
         list.displayList();
         System.out.println(list.size);
+        list.insertAtIndex(70,3);
+        list.displayList();
+        System.out.println(list.get(4));
+        list.deleteAtIndex(7);
+        list.displayList();
     }
 }
