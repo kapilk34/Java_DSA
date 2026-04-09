adb51877747601a72365e95b5b8b39cfc4f16a3a
kapilk34 <kapilkumargupta1511@gmail.com>
2026-04-09 14:22:29 +0530

    Some more practise questions

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -1,7 +1,24 @@
 package Arrays;
 
 public class SecondLargest {
+    public static int secondLargest(int[] arr){
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int num : arr){
+            if(num > largest){
+                second = largest;
+                largest = num;
+            }
+            else if(num > second && num != largest){
+                second = num;
+            }
+        }
+        return second == Integer.MIN_VALUE ? -1 : second;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {};
+        int[] arr = {2,34,4,7,87,99,23};
+        System.out.println(secondLargest(arr));
     }
 }
