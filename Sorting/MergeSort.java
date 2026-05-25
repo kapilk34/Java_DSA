58cc4f8c21e90230065cff6af6b3a88a4eca77e0
kapilk34 <kapilkumargupta1511@gmail.com>
2026-05-25 11:46:32 +0530

    Practice questions of sorting technique

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -46,3 +46,4 @@ public class MergeSort {
         System.out.println();
     }
 }
+
--- Sorting/countingSort.java
+++
@@ -1,7 +0,0 @@
-package Sorting;
-
-public class countingSort {
-    public static void main(String[] args) {
-        int[] nums = {2,2,2,1,1,5,4,3,3,5,6,6,7,2,2,9,4,5,5};
-    }
-}
